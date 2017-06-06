62aca7662022fd1a4984abd9426e2ae6e91817a2
Joseph Samela <josephsamela@gmail.com>
2017-06-05 20:27:01 -0400

    Added vault.bat to run on windows from cmd

--- vault.bat
+++ vault.bat
@@ -1,13 +1,11 @@
 @echo off
 
-REM   To enable lightning fast access in the form  "vault accountName"...
+REM To enable lightning fast access in the form  "vault accountName"...
 REM
-REM   1. Replace dir variable below with path to vault.py
-REM   2. move this file to ~/.bin folder
-REM   3. add -->   export PATH=$PATH":$HOME/.bin"  <-- to .bash_profile or .bash_rc file 
+REM 1. Replace path variable below  with path to vault.py
+REM 2. move this file to ~/.bin folder
+REM 3. add -->   export PATH=$PATH":$HOME/.bin"  <-- to .bash_profile or .bash_rc file 
 
-set dir=C:\Users\jkerman\GitHub\vault\vault.py
-
-set PATH=%PATH%;C:\Users\jkerman\AppData\Local\Continuum\Anaconda3
+set dir="C:\Users\JKerman\GitHub\vault\vault.py"
 
 python %dir% %1
